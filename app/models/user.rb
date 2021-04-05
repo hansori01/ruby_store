eb63b67c3b98ed0079526e308efdbbd497353e41
hansori01 <19sorihan88@gmail.com>
2021-04-05 22:19:07 +0000

    testing for user auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,21 @@
 class User < ActiveRecord::Base
   has_secure_password
+
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :password, presence: true
+  validates :password, length: {minimum: 5}
+  validates :password_confirmation, presence: true
+  validates_uniqueness_of :email, :case_sensitive => false
+  before_save { self.email.downcase!}
+
+  def self.authenticate_with_credentials(email, password)
+    user = User.find_by_email(email.downcase.strip)
+    if user && user.authenticate(password)
+      user
+    else
+      nil
+    end
+  end
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,14 +2,122 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
-  
+
   describe 'Validations' do
-    # validation examples here
+
+    it 'should create a user with all fields' do
+      user = User.create(
+        name: 'Bob',
+        email: 'bob@bob.bob',
+        password: 'hello',
+        password_confirmation: 'hello'
+      )
+      expect(user).to (be_valid)
+    end
+
+    it 'should be invalid without email' do
+      user = User.create(
+        name: 'Bob',
+        email: nil,
+        password: 'hello',
+        password_confirmation: 'hello'
+      )
+      expect(user).to_not (be_valid)
+    end
+
+    it 'should be invalid without password' do
+      user = User.create(
+        name: 'Bob',
+        email: 'bob@bob.bob',
+        password: nil,
+        password_confirmation: 'hello'
+      )
+      expect(user).to_not (be_valid)
+    end
+
+    it 'should be invalid with a password less than 5 characters' do
+      user = User.create(
+        name: 'Bob',
+        email: 'bob@bob.bob',
+        password: 'helo',
+        password_confirmation: 'helo'
+      )
+      expect(user).to_not (be_valid)
+    end
+
+    it 'should be invalid with mismatching password' do
+      user = User.create(
+        name: 'Bob',
+        email: 'bob@bob.bob',
+        password: 'adele',
+        password_confirmation: 'hello'
+      )
+      expect(user).to_not (be_valid)
+    end
+
+    it 'should be invalid without password confirmation' do
+      user = User.create(
+        name: 'Bob',
+        email: 'bob@bob.bob',
+        password: 'adele',
+        password_confirmation: nil
+      )
+      expect(user).to_not (be_valid)
+    end
+
+    it 'should be invalid with existing email' do
+      user1 = User.create(
+        name: 'Bob',
+        email: 'bob@bob.bob',
+        password: 'adele',
+        password_confirmation: 'adele'
+      )
+      user2 = User.create(
+        name: 'Bob2',
+        email: 'bob@bob.bob',
+        password: 'adele',
+        password_confirmation: 'adele'
+      )
+      expect(user2).to_not (be_valid)
+    end
   end
 
 
   describe '.authenticate_with_credentials' do
-    # examples for this class method here
+
+    it 'should authenticate a user' do
+      user = User.create(
+        name: 'Bob',
+        email: 'bob@bob.bob',
+        password: 'hello',
+        password_confirmation: 'hello'
+      )
+      expect(User.authenticate_with_credentials(user.email, user.password)).to eq(user)
+    end 
+
+    it 'should authenticate a with extra space in email' do
+      user = User.create(
+        name: 'Bob',
+        email: 'bob@bob.bob',
+        password: 'hello',
+        password_confirmation: 'hello'
+      )
+      spaced_email = '  bob@bob.bob      '
+      expect(User.authenticate_with_credentials(spaced_email, user.password)).to eq(user)
+    end 
+
+    it 'should authenticate a with wrong casing in email' do
+      user = User.create(
+        name: 'Bob',
+        email: 'bob@bob.bob',
+        password: 'hello',
+        password_confirmation: 'hello'
+      )
+      cased_email = 'bOb@bOb.bOb'
+      expect(User.authenticate_with_credentials(cased_email, user.password)).to eq(user)
+    end 
+
+
   end
 
 
