e4532c1c8266292ec6bfb1e119beb7cfc21d152c
hansori01 <19sorihan88@gmail.com>
2021-04-05 21:35:24 +0000

    initial user spec

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+
+  
+  describe 'Validations' do
+    # validation examples here
+  end
+
+
+  describe '.authenticate_with_credentials' do
+    # examples for this class method here
+  end
+
+
+end
