68b2d177b8f6add140ec7ff260bc047011e22980
hansori01 <19sorihan88@gmail.com>
2021-04-06 19:32:15 +0000

    basic testing for user to click on product and directed to info

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
+
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    1.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "Navigate to item" do
+    # ACT
+    visit root_path
+    expect(page).to have_css 'article.product', count: 1
+    click_on 'Details'
+    expect(page).to have_css 'dl.dl-horizontal', count: 1
+    save_screenshot
+    # DEBUG / VERIFY
+  end
+
+end
